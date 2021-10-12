ed6366c3663d3c1cf52f36a356a7c378ab2b70a0
unknown <nolan.beihl@gmail.com>
2021-10-12 10:25:37 -0600

    Starting the shopping cart

---
+++ product.py
@@ -0,0 +1,9 @@
+class Product:
+    def __init__(self, product_type, product_name, product_price):
+        product_type = []
+        product_name = []
+        product_price = []
+        self.product_name = product_name
+        self.product_type = product_type
+        self.product_price = product_price
+        
